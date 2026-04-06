64b3295c5c8ec937259334017a944477d84af7a9
Mohammad Boaisha <mohammad.boaisha@gmail.com>
2026-04-05 21:17:02 -0500

    I knew it! It was a SELinux problem

--- files/scripts/setup-greetd.sh
+++ files/scripts/setup-greetd.sh
@@ -16,3 +16,6 @@ cat > /usr/lib/tmpfiles.d/dms-greeter-dirs.conf << 'EOF'
 d /var/cache/dms-greeter 0750 greeter greeter -
 d /var/lib/greeter 0755 greeter greeter -
 EOF
+
+# SELinux screwing me again
+semanage permissive -a greetd_t 2>/dev/null || true
